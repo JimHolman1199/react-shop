b909b5739e293993512298f81e746ebc0df7f674
jimholman1199 <jimholman1199@gmail.com>
2020-07-22 10:50:00 +0300

    add HOC, move shop to firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -39,7 +39,37 @@ export const createUserProfileDoc = async (userAuth, additinalData) =>{
     }
 
     return userRef;
+};
 
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey);
+    
+    const batch = firestore.batch();
+    objectsToAdd.forEach(obj =>{
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef, obj)
+    })
+
+    return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformCollection = collections.docs.map(doc=>{
+        const {title, items} = doc.data()
+    
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id : doc.id,
+            title,
+            items
+        }
+    })
+
+   return transformCollection.reduce((acc, collection)=>{
+        acc[collection.title.toLowerCase()] = collection;
+        return acc;
+    }, {})
+    
 }
 
 export const auth = firebase.auth();
